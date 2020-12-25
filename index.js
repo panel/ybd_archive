7c8571319517f5ad518aa3c0a35dbc792b2001f6
Paul Nelson <panel@umich.edu>
2020-12-25 11:42:01 -0600

    add docsify to this

--- index.js
+++ index.js
@@ -1,40 +1,68 @@
-const fs = require('fs');
-const slugify = require('slugify');
-const rm = require('rimraf');
-const TurndownService = require('turndown');
-const posts = require('./data.json')[2].data;
-const REAMDE = fs.createWriteStream('./README.md');
-
+const fs = require("fs");
+const slugify = require("slugify");
+const rm = require("rimraf");
+const TurndownService = require("turndown");
+const posts = require("./data.json")[2].data;
 const turndownService = new TurndownService();
-const formatDate = date => {
-    let d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
 
-    if (month.length < 2) 
-        month = '0' + month;
-    if (day.length < 2) 
-        day = '0' + day;
+const formatDate = (date) => {
+  let d = new Date(date),
+    month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
 
-    return [year, month, day].join('-');
-}
+  if (month.length < 2) {
+    month = "0" + month;
+  }
+  if (day.length < 2) {
+    day = "0" + day;
+  }
 
-rm('posts', e => {
-    fs.mkdirSync('posts');
-    REAMDE.write(`# Yeastbound and Down Archives\n\n`);
-    posts.filter(p => p.post_type === 'post' && p.post_status === 'publish').forEach(post => {
-        const date = formatDate(post.post_date);
-        const cleanedUp = post.post_content.replace(/\r\n/g, '\n').replace(/\n\n/g, '</p><p>').replace(/\n/g, '<p>').replace(/\[caption .*(<.*\/>).*caption\]/g, '$1');
-        const content = turndownService.turndown(`<h1>${post.post_title}</h1>\n${cleanedUp}`);
-        const filePath = `./posts/${slugify([date, post.post_title].join(' '))}.md`;
-        fs.writeFileSync(filePath, content);
-        REAMDE.write(`* [${post.post_title}](${filePath})\n`);
+  return [year, month, day].join("-");
+};
+
+const categorize = (title) => {
+  return (/recipe/i.test(title))
+    ? "Recipes"
+    : (/review/i.test(title))
+    ? "Reviews"
+    : "Other";
+};
+
+rm("posts", (e) => {
+  const byCategory = {};
+  fs.mkdirSync("posts");
+  fs.mkdirSync("posts/recipes");
+  fs.mkdirSync("posts/reviews");
+  fs.mkdirSync("posts/other");
+  posts.filter((p) => p.post_type === "post" && p.post_status === "publish")
+    .forEach((post) => {
+      const date = formatDate(post.post_date);
+      const cleanedUp = post.post_content.replace(/\r\n/g, "\n").replace(
+        /\n\n/g,
+        "</p><p>",
+      ).replace(/\n/g, "<p>").replace(/\[caption .*(<.*\/>).*caption\]/g, "$1");
+      const content = turndownService.turndown(
+        `<h1>${post.post_title}</h1>\n${cleanedUp}`,
+      );
+      const category = categorize(post.post_title);
+      const filePath = `./posts/${category.toLowerCase()}/${
+        slugify([date, post.post_title].join(" ")).replace(/:/g, "")
+      }.md`;
+      fs.writeFileSync(filePath, content);
+      if (!byCategory[category]) byCategory[category] = [];
+      byCategory[category].push([post.post_title, filePath]);
     });
 
-    REAMDE.close();
-    fs.copyFileSync('./README.md', './index.md');
+  const readme = Object.entries(byCategory).reverse().reduce(
+    (page, [category, posts]) =>
+      page.concat([
+        `## ${category}`,
+        ...posts.map((p) => `* [${p[0]}](${p[1]})`),
+      ]),
+    [],
+  ).join("\n");
+  fs.writeFileSync("./README.md", readme);
+  fs.copyFileSync("./README.md", "./index.md");
 });
-const files = fs.readdirSync('posts');
-
-
+const files = fs.readdirSync("posts");
